84b6e71ec8a9a0206ab837ac3c78a73cb1e77b2b
Mombabil <mombabil.7@gmail.com>
2023-05-01 10:04:26 +0200

    edit bundle.php

--- config/bundles.php
+++ config/bundles.php
@@ -8,5 +8,5 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
-    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true]
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
